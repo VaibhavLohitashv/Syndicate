d367b7af05b36f199055666ac6933a9d40cb40d6
Vaibhav Lohitashv <vaibhav_lohitashv@epam.com>
2025-03-25 17:28:22 +0530

    v2

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -2,9 +2,10 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello from Lambda'
-        })
+        body: {
+            statusCode: 200,
+            message: "Hello from Lambda"
+        },
     };
     return response;
 };
