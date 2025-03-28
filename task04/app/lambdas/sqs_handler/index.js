f23fccd860af4aaecf96df383a1a541ffea7224b
Vaibhav Lohitashv <vaibhav_lohitashv@epam.com>
2025-03-28 23:54:07 +0530

    task 4 v5

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,8 +1,22 @@
+const util = require('util');
+
+/**
+ * CloudWatch Log Handler for SQS messages.
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    try {
+        // Log the contents of each message in the SQS batch
+        event.Records.forEach(record => {
+            const messageBody = record.body || '';
+            console.log(`SQS Message Body: ${messageBody}`);
+        });
+
+        return {
+            statusCode: 200,
+            body: 'SQS Message successfully logged.',
+        };
+    } catch (error) {
+        console.error(`An error occurred while processing SQS messages: ${error.message}`);
+        throw error;
+    }
 };
