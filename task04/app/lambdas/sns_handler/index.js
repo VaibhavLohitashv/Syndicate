5937c6a8b6f94c9a1d680713d19edf24177f1910
Vaibhav Lohitashv <vaibhav_lohitashv@epam.com>
2025-03-29 13:24:09 +0530

    task 4 v6

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,8 +1,22 @@
+const util = require('util');
+
+/**
+ * CloudWatch Log Handler for SNS messages.
+ */
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+    try {
+        // Log the contents of SNS messages
+        event.Records.forEach(record => {
+            const snsMessage = record.Sns.Message || '';
+            console.log(`SNS Message Content: ${snsMessage}`);
+        });
+
+        return {
+            statusCode: 200,
+            body: 'SNS Message successfully logged.',
+        };
+    } catch (error) {
+        console.error(`An error occurred while processing SNS messages: ${error.message}`);
+        throw error;
+    }
 };
